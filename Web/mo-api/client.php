00800c675606b412ad1b34f98f39f3e91d774ad4
moycat <mxq.1203@gmail.com>
2015-12-07 10:27:15 +0800

    Update

---
+++ Web/mo-api/client.php
@@ -0,0 +1,25 @@
+<?php
+	/*
+	 * mo-api/client.php @ MoyOJ
+	 * 
+	 * This file provides apis of communication with the
+	 * database through http.
+	 * 
+	 */
+	
+	function heart_beat()
+	{
+		
+	}
+	
+	function get_data()
+	{
+		
+	}
+	
+	function update_data()
+	{
+		
+	}
+	
+	
---
+++ Web/mo-api/mobile.php
@@ -0,0 +1,9 @@
+<?php
+	/*
+	 * mo-api/mobile.php @ MoyOJ
+	 * 
+	 * This file provides apis of communication with the
+	 * mobile app.
+	 * 
+	 */
+	
