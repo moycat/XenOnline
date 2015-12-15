def56fd291ba7400b753242e8c540d6ea56fa424
moycat <mxq.1203@gmail.com>
2015-12-15 14:03:08 +0800

    缓存优化

--- Web/mo-includes/function-data.php
+++ Web/mo-includes/function-data.php
@@ -70,63 +70,81 @@
 			$db->bind( 'ss', $option, $to_write );
 		}
 		$db->execute();
-		mo_del_cache( 'mo_cache_settings' );
+		mo_write_cache( 'mo_cache_settings', $mo_settings );
 		mo_write_note( "Site option: '$option' has been update." );
 		return $rt;
 	}
 	
 	function mo_get_solution_count( $pid = 'all', $uid = 'all', $state = 'all' )
 	{
-		global $db;
-		$sql = 'SELECT COUNT(*) AS total FROM mo_judge_solution WHERE 1=1';
-		if ( is_numeric( $pid ) )
+		$count = mo_read_cache( 'mo_prob_solution' );
+		if ( !$count )
 		{
-			$sql .= " AND `pid` = $pid";
+			global $db;
+			$sql = 'SELECT COUNT(*) AS total FROM mo_judge_solution WHERE 1=1';
+			if ( is_numeric( $pid ) )
+			{
+				$sql .= " AND `pid` = $pid";
+			}
+			if ( is_numeric( $uid ) )
+			{
+				$sql .= " AND `uid` = $uid";
+			}
+			if ( is_numeric( $state ) )
+			{
+				$sql .= " AND `state` = $state";
+			}
+			$db->prepare( $sql );
+			$result = $db->execute();
+			$count = (int)$result[0]['total'];
+			mo_write_cache( 'mo_solution_count', $count );
 		}
-		if ( is_numeric( $uid ) )
-		{
-			$sql .= " AND `uid` = $uid";
-		}
-		if ( is_numeric( $state ) )
-		{
-			$sql .= " AND `state` = $state";
-		}
-		$db->prepare( $sql );
-		$result = $db->execute();
-		return (int)$result[0]['total'];
+		return $count;
 	}
 	function mo_get_problem_count( $tag = '' )
 	{
-		global $db;
-		$sql = 'SELECT COUNT(*) AS total FROM `mo_judge_problem` WHERE `state` = 1';
-		if ( $tag )
+		$count = mo_read_cache( 'mo_problem_count' );
+		if ( !$count )
 		{
-			$sql .= ' AND (MATCH (tag) AGAINST (?))';
-			$db->prepare( $sql );
-			$db->bind( 's', $tag );
+			global $db;
+			$sql = 'SELECT COUNT(*) AS total FROM `mo_judge_problem` WHERE `state` = 1';
+			if ( $tag )
+			{
+				$sql .= ' AND (MATCH (tag) AGAINST (?))';
+				$db->prepare( $sql );
+				$db->bind( 's', $tag );
+			}
+			else
+			{
+				$db->prepare( $sql );
+			}
+			$result = $db->execute();
+			$count = (int)$result[0]['total'];
+			mo_write_cache( 'mo_problem_count', $count );
 		}
-		else
-		{
-			$db->prepare( $sql );
-		}
-		$result = $db->execute();
-		return (int)$result[0]['total'];
+		return $count;
 	}
 	function mo_get_discussion_count( $parent = 0, $category = 'all', $uid = 'all', $status = 1 )
 	{
-		global $db;
-		$start -= 1;
-		$sql = 'SELECT COUNT(*) AS total FROM `mo_discussion`  WHERE `parent` = ? AND `status` = ?';
-		if ( is_numeric( $category ) )
+		$count = mo_read_cache( 'mo_discussion_count' );
+		if ( !$count )
 		{
-			$sql .= " AND `category` = $category";
+			global $db;
+			$start -= 1;
+			$sql = 'SELECT COUNT(*) AS total FROM `mo_discussion`  WHERE `parent` = ? AND `status` = ?';
+			if ( is_numeric( $category ) )
+			{
+				$sql .= " AND `category` = $category";
+			}
+			if ( is_numeric( $uid ) )
+			{
+				$sql .= " AND `uid` = $uid";
+			}
+			$db->prepare( $sql );
+			$db->bind( 'ii', $parent, $status );
+			$result = $db->execute();
+			$count = (int)$result[0]['total'];
+			mo_write_cache( 'mo_discussion_count', $count );
 		}
-		if ( is_numeric( $uid ) )
-		{
-			$sql .= " AND `uid` = $uid";
-		}
-		$db->prepare( $sql );
-		$db->bind( 'ii', $parent, $status );
-		$result = $db->execute();
-		return (int)$result[0]['total'];
+		return $count;
 	}
