07df66cb472622ab401476f084572114a7c8d478
moycat <mxq.1203@gmail.com>
2016-02-28 19:49:48 +0800

    begin: a new start
    开始新版本的开发。
    原先的Web程序将会完全弃用。

--- Web/mo-admin/hitokoto.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$url = 'http://api.hitokoto.us/rand?encode=js&charset=utf-8';
-$contents = file_get_contents($url);
-header('Content-Type: text/javascript;charset=utf-8');
-$contents = explode('"', $contents);
-$contents = 'function hitokoto(){$(".hitokoto").append("'.$contents[1].'");}';
-echo $contents;
